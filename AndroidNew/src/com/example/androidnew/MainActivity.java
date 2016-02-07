355c392ded479f40c36af8b7787bcee9da9d0d68
manas <pmanas88@gmail.com>
2016-02-08 00:51:49 +0530

    Added Log to MainActivity

--- AndroidNew/src/com/example/androidnew/MainActivity.java
+++ AndroidNew/src/com/example/androidnew/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Log.d("I am in MainActivity");
 	}
 
 	@Override
